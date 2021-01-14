c1136627d764980f083179d5acb05132cd559bef
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-13 22:08:38 -0800

    commit tree 60d042f5d716e858d785be896ca5b081cf941673

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def name
+        @name
+    end
+    def roster
+        @roster
+    end
+    def add_student(student_name, grade)
+        if @roster[grade]
+           @roster[grade].push(student_name)
+        else 
+            @roster[grade] = [student_name]
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.each do |key, value| value.sort!
+        end
+    end
+end
